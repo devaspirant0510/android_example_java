e30e29fa6c8d4152652b5604115720b63b6b7566
devaspirant0510 <dltmdgh2@sonline20.sen.go.kr>
2020-12-27 23:08:50 +0900

    add PrograssBar Thread & Handler Example

---
+++ PrograssBar/app/src/main/java/com/example/progressbar/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.progressbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    ProgressBar progressBar;
+    Handler handler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        progressBar = findViewById(R.id.progressBar);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ProgressThread thread = new ProgressThread();
+                        thread.start();
+                    }
+                },3000);
+            }
+        });
+    }
+
+    class ProgressThread extends Thread{
+        boolean run = false;
+        int values = 0;
+        public void run(){
+            run = true;
+            while(run){
+                if (values>100){
+                    break;
+                }
+                values+=1;
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setProgress(values);
+                    }
+                });
+                try {
+                    Thread.sleep(250);
+                }catch (Exception e){}
+
+
+            }
+        }
+    }
+
+}
