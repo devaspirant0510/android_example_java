d1a765dc3d4eb2a96a2172e9a3597941cde2744d
devaspirant0510 <dltmdgh2@sonline20.sen.go.kr>
2020-12-30 17:21:12 +0900

    update Prograss bar Thread handler Runnable

--- PrograssBar/app/src/main/java/com/example/progressbar/MainActivity.java
+++ PrograssBar/app/src/main/java/com/example/progressbar/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.progressbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -6,11 +7,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     ProgressBar progressBar;
+    TextView progress_text;
     Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +21,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
         progressBar = findViewById(R.id.progressBar);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 handler.postDelayed(new Runnable() {
                     @Override
+
                     public void run() {
-                        ProgressThread thread = new ProgressThread();
-                        thread.start();
+//                        ProgressThread thread = new ProgressThread();
+//                        thread.start();
+                        new Thread(new Runnable() {
+                            boolean run =false;
+                            int values = 0;
+                            @Override
+                            public void run() {
+                                run = true;
+                                while(run){
+                                    if (values>=100){
+                                        break;
+                                    }
+                                    values+=1;
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            progressBar.setProgress(values);
+
+                                        }
+                                    });
+                                    try {
+                                        Thread.sleep(250);
+                                    }catch (Exception e){}
+
+
+                                }
+                            }
+
+                        }).start();
                     }
                 },3000);
             }
@@ -38,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         public void run(){
             run = true;
             while(run){
-                if (values>100){
+                if (values>=100){
                     break;
                 }
                 values+=1;
@@ -46,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         progressBar.setProgress(values);
+
                     }
                 });
                 try {
