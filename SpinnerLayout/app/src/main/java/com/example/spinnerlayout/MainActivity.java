321cbc04122eaeb686a6b27a155b84de7d1be06e
devaspirant0510 <dltmdgh2@sonline20.sen.go.kr>
2021-01-11 23:58:12 +0900

    update spinner layout example setOnItemListner

--- SpinnerLayout/app/src/main/java/com/example/spinnerlayout/MainActivity.java
+++ SpinnerLayout/app/src/main/java/com/example/spinnerlayout/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.spinnerlayout;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView result;
+    private TextView result,tv;
     private Spinner spinner;
+    private Button plus;
+    ArrayAdapter<String> arrayAdapter;
+    List<String> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +34,19 @@ public class MainActivity extends AppCompatActivity {
 
         spinner = (Spinner)findViewById(R.id.spinner);
         result = (TextView)findViewById(R.id.result);
+        int sum = spinner.getCount();
+        plus = (Button)findViewById(R.id.plus);
+        tv = (TextView)findViewById(R.id.tv);
+        list = new ArrayList<>();
+        for (int i = 0; i < spinner.getCount(); i++) {
+            list.add(String.valueOf(spinner.getItemAtPosition(i)));
 
+        }
+        list.add("aaa");
+        list.add("bb");
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,list);
+        spinner.setAdapter(arrayAdapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -31,5 +59,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show();
+
+            }
+        });
+
+
+    }
+    public void show(){
+
+        final EditText edittext = new EditText(getApplicationContext());
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("AlertDialog Title");
+        builder.setMessage("AlertDialog Content");
+        builder.setView(edittext);
+        builder.setPositiveButton("입력",
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(getApplicationContext(),edittext.getText().toString() ,Toast.LENGTH_LONG).show();
+                    tv.setText(edittext.getText().toString());
+                    list.add(edittext.getText().toString());
+                    arrayAdapter.notifyDataSetChanged();
+                }
+            });
+        builder.setNegativeButton("취소",
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+        builder.show();
     }
 }
