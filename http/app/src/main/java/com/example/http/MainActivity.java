86fa0b723ed09d7907d4d220761747e744faf65a
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-01-28 22:54:58 +0900

    update http conection

--- http/app/src/main/java/com/example/http/MainActivity.java
+++ http/app/src/main/java/com/example/http/MainActivity.java
@@ -13,93 +13,95 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.ThreadPoolExecutor;
 
 public class MainActivity extends AppCompatActivity {
-    TextView log;
-    EditText etext;
+    TextView tv_log;
+    EditText et_url;
     ScrollView scrollView;
-     //requestHandler handler = new requestHandler();
+    //requestHandler handler = new requestHandler();
     Handler handler = new Handler();
     Button button;
+    String line = null;
+    String getUrl;
+    public static String Tag = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etext = findViewById(R.id.etext);
+        et_url = findViewById(R.id.etext);
         button = findViewById(R.id.button);
         scrollView = findViewById(R.id.view);
-        log = findViewById(R.id.log);
+        tv_log = findViewById(R.id.log);
+        getUrl = et_url.getText().toString();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        new Thread(new Runnable() {
-                            @Override
-                            public void run() {
-                                String getText = etext.getText().toString();
-                                try{
-                                    URL url = new URL(getText);
-                                    HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-                                    if (connection!=null){
-                                        connection.setConnectTimeout(10000);
-                                        connection.setRequestMethod("GET");
-                                        connection.setDoInput(true);
-                                        connection.setDoOutput(true);
-                                        int requestCode = connection.getResponseCode();
-                                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                                        String line = null;
-
-                                        while (true){
-                                            line = bufferedReader.readLine();
-                                            if (line==null){
-                                                break;
-                                            }
-                                            println(line);
-                                        }
-                                    }
-                                }catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-
-                            }
-                        }).start();
-                    }
-                });
-                /*RequestThread requestThread = new RequestThread();
-                requestThread.start();*/
+                RequestThread requestThread = new RequestThread();
+                requestThread.start();
             }
         });
 
     }
-    public void println(String data){
-        //log.append(data+"\n");
-        Bundle bundle = new Bundle();
-        bundle.putString("data",data);
-        Message msg  = handler.obtainMessage();
-        msg.setData(bundle);
-        Log.e("adsf",data);
+
+    public void println(final String data) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                tv_log.append(data + "\n");
+            }
+        });
+
+        Log.e("adsf", data);
     }
-    class RequestThread extends Thread{
+
+    class RequestThread extends Thread {
         public void run() {
+            try {
+                URL url = new URL(getUrl);
+                Log.e(Tag,url+"");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                if (connection != null) {
+                    connection.setConnectTimeout(100000);
+                    connection.setRequestMethod("GET");
+                    connection.getDoInput();
+                    connection.getDoOutput();
+
+                    int reCode = connection.getResponseCode();
+                    Log.e(Tag,reCode+"");
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    while (true) {
+                        line = reader.readLine();
+                        if (line == null) {
+                            break;
+                        }
+
+                        println(line);
+                    }
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         }
     }
-    class requestHandler extends Handler{
+
+    class requestHandler extends Handler {
         @Override
         public void handleMessage(@NonNull Message msg) {
             super.handleMessage(msg);
             Bundle get = msg.getData();
             String getdata = get.getString("data");
-            log.setText(getdata);
+            tv_log.setText(getdata);
         }
     }
 }
