067caed923a8a68dca9de2674f4ac78c9403bb54
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-02-02 00:45:28 +0900

    update SharedPreference Exaple

--- sharedpreferences/app/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ sharedpreferences/app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -6,52 +6,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.sharedpreferences.R;
 import com.example.sharedpreferences.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mb;
-    private View root;
     private int m_score,m_level;
 
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt("level",m_level);
-        outState.putInt("score",m_score);
-
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        SharedPreferences sharedPreferences = getSharedPreferences("gson",0);
-        SharedPreferences.Editor editor =sharedPreferences.edit();
-        String value = mb.etTest.getText().toString();
-        editor.putString("value",value);
-        editor.apply();
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mb = ActivityMainBinding.inflate(getLayoutInflater());
-        root = mb.getRoot();
+        View root = mb.getRoot();
         setContentView(root);
-        if (savedInstanceState!=null){
-            m_level=savedInstanceState.getInt("level");
-            m_score=savedInstanceState.getInt("score");
-            mb.tvLevel.setText("level : "+m_level);
-            mb.tvScore.setText("socre : "+m_score);
-        }
-        SharedPreferences sharedPreferences = getSharedPreferences("gson",0);
-        String get = sharedPreferences.getString("value","null");
-        mb.etTest.setText(get);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("shared",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        String userName = sharedPreferences.getString("name","");
+        mb.etTest.setText(userName);
+        m_level = sharedPreferences.getInt("level",0);
+        m_score = sharedPreferences.getInt("score",0);
+        mb.tvLevel.setText("Level : "+m_level);
+        mb.tvScore.setText("Score : "+m_score);
         mb.btnLevel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 m_level+=1;
-                mb.tvLevel.setText("level : "+m_level);
+                mb.tvLevel.setText("Level : "+m_level);
 
             }
         });
@@ -59,11 +46,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 m_score+=1;
-                mb.tvScore.setText("score : "+m_score);
+                mb.tvScore.setText("Score : "+m_score);
             }
         });
 
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences sharedPreferences = getSharedPreferences("shared",MODE_PRIVATE);
+        SharedPreferences.Editor editor =sharedPreferences.edit();
+        String value = mb.etTest.getText().toString();
+        editor.putString("name",value);
+        editor.putInt("level",m_level);
+        editor.putInt("score",m_score);
+        editor.apply();
+
+    }
 
 }
