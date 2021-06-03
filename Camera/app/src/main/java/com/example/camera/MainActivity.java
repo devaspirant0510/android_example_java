a90f5d282d11bf46750bd33e59367ac299a14b45
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-06-04 00:39:44 +0900

    OnActivityResult에서 이미지뷰 가져올때 오류 해결

--- Camera/app/src/main/java/com/example/camera/MainActivity.java
+++ Camera/app/src/main/java/com/example/camera/MainActivity.java
@@ -37,22 +37,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int permissonCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-        if (permissonCheck == PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(getApplicationContext(), "SMS 수신권한 있음", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(getApplicationContext(), "SMS 수신권한 없음", Toast.LENGTH_SHORT).show();
-            //권한설정 dialog에서 거부를 누르면 //ActivityCompat.shouldShowRequestPermissionRationale 메소드의 반환값이 true가 된다.
-            // 단, 사용자가 "Don't ask again"을 체크한 경우 //거부하더라도 false를 반환하여, 직접 사용자가 권한을 부여하지 않는 이상, 권한을 요청할 수 없게 된다.
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-                //이곳에 권한이 왜 필요한지 설명하는 Toast나 dialog를 띄워준 후, 다시 권한을 요청한다.
-                Toast.makeText(getApplicationContext(), "SMS권한이 필요합니다", Toast.LENGTH_SHORT).show();
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(),"권한 설정 완료",Toast.LENGTH_SHORT).show();
             } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+                Toast.makeText(getApplicationContext(),"권한 설정 요청",Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
             }
         }
-
         File sdcard = Environment.getExternalStorageDirectory();
         SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY_MM_dd_hh");
         file = new File(sdcard, "mycapture.jpg");
@@ -87,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Log.e("d", "onActivityResult: d" );
-        if (requestCode == RESULT_OK) {
+        if (resultCode == RESULT_OK) {
             Log.e("d", "onActivityResult: d" );
             if (requestCode == 101) {
                 BitmapFactory.Options options = new BitmapFactory.Options();
